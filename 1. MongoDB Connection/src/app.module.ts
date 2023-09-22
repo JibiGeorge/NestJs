406f62cbacc2c817386bd9dfaa0ff4099f249158
Jibi George <jibiyyan@gmail.com>
2023-09-22 17:22:49 +0530

    MongoDB setup/Config

---
+++ 1. MongoDB Connection/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
